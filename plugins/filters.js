db8d9c6c5edac7cd801c34db2f1309cb0f0eae5c
madhav30092006 <87229928+madhav30092006@users.noreply.github.com>
2021-07-27 22:10:16 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAsena - Madhav MN
 */
 const fs = require('fs')
 const Asena = require('../events');
